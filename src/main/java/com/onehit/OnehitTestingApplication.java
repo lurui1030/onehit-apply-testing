5fea98145a1e1965fcecf5d54e29840cfbe66764
lurui1030 <lurui1030@gmail.com>
2017-02-21 21:16:56 -0500

    first commit for testing

---
+++ src/main/java/com/onehit/OnehitTestingApplication.java
@@ -0,0 +1,12 @@
+package com.onehit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnehitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnehitTestingApplication.class, args);
+	}
+}
